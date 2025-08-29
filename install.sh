e02fbf1e13b8c1c2ed5ae716131af0d4a63597dd
Nathan <johnsonn28@gmail.com>
2025-08-29 15:03:18 -0600

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+mkdir -p /home/$USER/.config/nvim/
+cp init.lua /home/$USER/.config/nvim/init.lua
+
 nvim -c 'PackerSync'
